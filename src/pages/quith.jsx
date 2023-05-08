4bb25eed264c5d7f68e0b65d9bcf92dffa7d9be4
EAGLEMadaminjonov <javohirweb99@gamil.com>
2023-05-08 16:14:04 +0500

    setting re-render

--- src/pages/quith.jsx
+++ src/pages/quith.jsx
@@ -4,9 +4,6 @@ import { useGlobalContext } from "../context.jsx";
 function Questions() {
   const { info, loading } = useGlobalContext();
   const [numItem, setNumItem] = useState(0);
-  const [newInfo, setNewInfo] = useState([]);
-  const [showItem, setShowItem] = useState(false);
-  setNewInfo(info.slice(numItem, 1));
 
   console.log(numItem);
   if (loading) {
@@ -32,53 +29,47 @@ function Questions() {
       </div>
     );
   }
+  let newInfo = info.slice(numItem, 1);
   return (
     <div className="bg-[linear-gradient(90deg,#5041b2,#7969e6)] py-5 ">
       <h1 className="text-3xl font-bold text-center text-white">
         Answer on Questions
       </h1>
       {newInfo.map((item, index) => {
-        if (numItem === index) {
-          setShowItem(true);
-        }
-        {
-          showItem ? (
-            <div
-              key={index}
-              className=" px-3 relative pt-2 pb-2 w-[500px] mx-auto border-2 lg:w-[700px] rounded m-2"
-            >
-              <p className="absolute right-8 text-white text-[12px]">
-                {numItem + 1} out of {info.length}
-              </p>
-              <h2 className="text-xl text-white p-3 mt-3">
-                {index + 1} {item.question}
-              </h2>
-              <div className="flex flex-col ">
-                {item.incorrect_answers.map((one, ibtn) => {
-                  return (
-                    <button
-                      key={ibtn}
-                      onClick={() => setNumItem(index + 1)}
-                      className="border border-white m-2 p-1 px-3 text-left text-white rounded hover:bg-[#7666E2] active:bg-red-400"
-                    >
-                      {one}
-                    </button>
-                  );
-                })}
-                <button
-                  onClick={() => {
-                    setNumItem(index + 1);
-                  }}
-                  className="border border-white text-left p-1 px-3 text-white m-2 rounded hover:bg-[#7666E2] active:bg-green-300"
-                >
-                  {item.correct_answer}
-                </button>
-              </div>
+        return (
+          <div
+            key={index}
+            className=" px-3 relative pt-2 pb-2 w-[500px] mx-auto border-2 lg:w-[700px] rounded m-2"
+          >
+            <p className="absolute right-8 text-white text-[12px]">
+              {numItem + 1} out of {info.length}
+            </p>
+            <h2 className="text-xl text-white p-3 mt-3">
+              {index + 1} {item.question}
+            </h2>
+            <div className="flex flex-col ">
+              {item.incorrect_answers.map((one, ibtn) => {
+                return (
+                  <button
+                    key={ibtn}
+                    onClick={() => setNumItem(index + 1)}
+                    className="border border-white m-2 p-1 px-3 text-left text-white rounded hover:bg-[#7666E2] active:bg-red-400"
+                  >
+                    {one}
+                  </button>
+                );
+              })}
+              <button
+                onClick={() => {
+                  setNumItem(index + 1);
+                }}
+                className="border border-white text-left p-1 px-3 text-white m-2 rounded hover:bg-[#7666E2] active:bg-green-300"
+              >
+                {item.correct_answer}
+              </button>
             </div>
-          ) : (
-            ""
-          );
-        }
+          </div>
+        );
       })}
     </div>
   );
